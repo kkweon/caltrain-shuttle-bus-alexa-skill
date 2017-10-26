9760f6c747959880759e3c80ce42d08894367a3c
KYUNG MO KWEON <kkweon@gmail.com>
2017-10-25 21:20:51 -0700

    Init project

---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * @fileOverview AWS Lambda Alexa Skill
+ * @name index.js
+ * @author Mo Kweon
+ * @license MIT
+ */
+
+"use strict";
+
+const Alexa = require('alexa-sdk');
+const get_next_shuttlebus = require("./shuttle-bus");
+
+const APP_ID = "amzn1.ask.skill.22c8922c-21d3-4a41-891f-bddf14e34e89";
+
+let messageBuilder = (message, time) => {
+    return message + ` at ${time}`;
+};
+
+const messages = {
+    HELP_MESSAGE: "You can say when is the next shuttle bus?"
+};
+
+const handlers = {
+    'LaunchRequest': function() {
+        this.emit('GetShuttleBusSchedule');
+    },
+
+    'GetShuttleBusSchedule': function() {
+        const response = get_next_shuttlebus();
+        if (response.shuttle_bus) {
+            const message = messageBuilder(response.message, response.shuttle_bus[0]);
+            this.emit(":tell", message);
+        } else {
+            this.emit(":tell", response.message);
+        }
+    },
+
+    'AMAZON.HelpIntent': function() {
+        const speechOutput = messages.HELP_MESSAGE;
+        const reprompt = messages.HELP_MESSAGE;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function() {
+        this.emit(':tell', "Bye");
+    },
+    'AMAZON.StopIntent': function() {
+        this.emit(':tell', "Good Bye");
+    }
+};
+
+exports.handler = function(event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
