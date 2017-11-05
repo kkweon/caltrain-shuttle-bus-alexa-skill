2642441c3cf8f699667b2624f779fd427dc1aaab
KYUNG MO KWEON <kkweon@gmail.com>
2017-11-05 15:11:24 -0800

    PDT -> PST

--- shuttlebus.js
+++ shuttlebus.js
@@ -13,41 +13,41 @@
  * @returns {number[]} Returns [Hour, Minute]
  */
 function getPacificTime(date) {
-    // PDT + 7 = UTC => PDT = UTC - 7;
-    // So, we subtract 7 hours and get the UTC Time.
-    date.setTime(date.getTime() - 7 * 60 * 60 * 1000);
+  // PDT + 7 = UTC => PDT = UTC - 7;
+  // PST + 8 = UTF => PDT = UTC - 8;
+  date.setTime(date.getTime() - 8 * 60 * 60 * 1000);
 
-    return [date.getUTCHours(), date.getUTCMinutes()];
+  return [date.getUTCHours(), date.getUTCMinutes()];
 }
 
 // Morning Shuttle Bus Schedule
 // [[hour, minutes]] in PST
 const morning_schedule = [
-    [6, 10],
-    [6, 26],
-    [6, 38],
-    [6, 52],
-    [7, 20],
-    [7, 37],
-    [7, 52],
-    [8, 17],
-    [8, 35],
-    [8, 52],
-    [9, 12]
+  [6, 10],
+  [6, 26],
+  [6, 38],
+  [6, 52],
+  [7, 20],
+  [7, 37],
+  [7, 52],
+  [8, 17],
+  [8, 35],
+  [8, 52],
+  [9, 12]
 ];
 
 // Afternoon schedule
 const afternoon_schedule = [
-    [3, 43],
-    [4, 3],
-    [4, 39],
-    [5, 7],
-    [5, 29],
-    [5, 47],
-    [6, 6],
-    [6, 27],
-    [6, 45],
-    [7, 5]
+  [3, 43],
+  [4, 3],
+  [4, 39],
+  [5, 7],
+  [5, 29],
+  [5, 47],
+  [6, 6],
+  [6, 27],
+  [6, 45],
+  [7, 5]
 ].map(([hour, minutes]) => [hour + 12, minutes]);
 
 const schedule = morning_schedule.concat(afternoon_schedule);
@@ -59,13 +59,13 @@ const schedule = morning_schedule.concat(afternoon_schedule);
  * @returns {Bool} IF time_x < time_y, then TRUE
  */
 function compareDate([h_x, m_x], [h_y, m_y]) {
-    if (h_x < h_y) {
-        return true;
-    } else if (h_x == h_y && m_x <= m_y) {
-        return true;
-    } else {
-        return false;
-    }
+  if (h_x < h_y) {
+    return true;
+  } else if (h_x == h_y && m_x <= m_y) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -75,7 +75,7 @@ function compareDate([h_x, m_x], [h_y, m_y]) {
  * @returns {number[][]} Available schedules [[h_x, m_x], ...].
  */
 function getAvailableList(schedule, current_time) {
-    return schedule.filter(time => compareDate(current_time, time));
+  return schedule.filter(time => compareDate(current_time, time));
 }
 
 /**
@@ -85,9 +85,9 @@ function getAvailableList(schedule, current_time) {
  * @returns {number} Time difference in Minutes
  */
 function getTimeDiff([h_x, m_x], [h_y, m_y]) {
-    let time1 = h_x * 60 + m_x;
-    let time2 = h_y * 60 + m_y;
-    return time1 - time2;
+  let time1 = h_x * 60 + m_x;
+  let time2 = h_y * 60 + m_y;
+  return time1 - time2;
 }
 
 /**
@@ -96,49 +96,46 @@ function getTimeDiff([h_x, m_x], [h_y, m_y]) {
  * @returns {string} Return description.
  */
 function getMessage(diff) {
-    let message = "";
-    if (diff > 60) {
-        let hour_diff = Math.floor(diff / 60);
-        let min_diff = diff % 60;
-        message = `Your next shuttle bus arrives in ${hour_diff} hour ${min_diff} minutes`;
-    } else {
-        message = `Your next shuttle bus arrives in ${diff} minutes`;
-    }
-    return message;
+  let message = "";
+  if (diff > 60) {
+    let hour_diff = Math.floor(diff / 60);
+    let min_diff = diff % 60;
+    message = `Your next shuttle bus arrives in ${hour_diff} hour ${min_diff} minutes`;
+  } else {
+    message = `Your next shuttle bus arrives in ${diff} minutes`;
+  }
+  return message;
 }
 
-
 function timeFormat([hour, minutes]) {
-    let am_or_pm = hour >= 12 ? "PM" : "AM";
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-    return `${hour % 12}:${minutes} ${am_or_pm}`;
+  let am_or_pm = hour >= 12 ? "PM" : "AM";
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  return `${hour % 12}:${minutes} ${am_or_pm}`;
 }
 
 function getNextSchedule(current_time, schedule) {
-    let available_list = getAvailableList(schedule, current_time);
-    if (available_list.length > 0) {
-        let time_diff = getTimeDiff(available_list[0], current_time);
-        return {
-            message: getMessage(time_diff),
-            shuttle_bus: available_list.map(timeFormat)
-        };
-    } else {
-        return {
-            message: "No shuttle bus is available as of " + timeFormat(current_time),
-            shuttle_bus: null
-        };
-    }
-
+  let available_list = getAvailableList(schedule, current_time);
+  if (available_list.length > 0) {
+    let time_diff = getTimeDiff(available_list[0], current_time);
+    return {
+      message: getMessage(time_diff),
+      shuttle_bus: available_list.map(timeFormat)
+    };
+  } else {
+    return {
+      message: "No shuttle bus is available as of " + timeFormat(current_time),
+      shuttle_bus: null
+    };
+  }
 }
 
-
 module.exports = {
-    schedule: schedule,
-    compareDate: compareDate,
-    getPacificTime: getPacificTime,
-    getAvailableList: getAvailableList,
-    getTimeDiff: getTimeDiff,
-    getNextSchedule: getNextSchedule
+  schedule: schedule,
+  compareDate: compareDate,
+  getPacificTime: getPacificTime,
+  getAvailableList: getAvailableList,
+  getTimeDiff: getTimeDiff,
+  getNextSchedule: getNextSchedule
 };
